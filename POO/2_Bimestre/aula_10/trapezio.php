93f3810b5e20154ed0a7e94370463c27d99374e4
KASSIME <giovana.kassime.tds2023@gmail.com>
2024-09-05 15:16:38 -0300

    posto poo

--- POO/2_Bimestre/aula_10/trapezio.php
+++ POO/2_Bimestre/aula_10/trapezio.php
@@ -5,7 +5,18 @@ class  Trapezio {
     private $baseMenor;
     private $altura;
     
-
+    public function __construct($baseMaior,$baseMenor,$altura) {
+        $this->baseMaior = $baseMaior;
+        $this->baseMenor = $baseMenor;
+        $this->altura = $altura;
+    }
+    public function __toString()
+    {
+        return "Trapezio com base maior de $this->baseMaior, base menor $this->baseMenor e altura igual a $this->altura";
+    }
+    public function calcArea(){
+        return (($this->baseMaior + $this->baseMenor) * $this->altura)/2;   
+    }
     /**
      * Get the value of baseMaior
      */
@@ -60,3 +71,23 @@ class  Trapezio {
         return $this;
     }
 }
+
+
+$trapezios = array();
+
+for ($i=0; $i < 3; $i++) { 
+    $baseMaior =  readline("Digite a base maior do Trapezio ". ($i+1).": "); 
+    $baseMenor =  readline("Digite a base menor do Trapezio ". ($i+1).": "); 
+    $altura =  readline("Digite a altura do Trapezio ". ($i+1).": ");
+    $trapezio = new Trapezio($baseMaior,$baseMenor,$altura);
+    array_push($trapezios,$trapezio);
+}
+
+$trapezioMaior = $trapezios[1];
+foreach ($trapezios as $trapezio) {
+    if ($trapezio->calcArea() > $trapezioMaior->calcArea()) {
+        $trapezioMaior = $trapezio;
+    }
+}
+
+echo "\nO trapézio que possui a maior área é \n".$trapezioMaior; 
