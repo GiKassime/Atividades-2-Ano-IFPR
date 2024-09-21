45c30f06a76cad153289fc67e45a363cc7507759
kassime <giovana.kassime.tds2023@gmail.com>
2024-09-21 14:08:40 -0300

    aaa

---
+++ POO/2_Bimestre/aula_11/PaisAtletas/modelos/Atleta.php
@@ -0,0 +1,10 @@
+<?php 
+require_once 'Pais.php';
+class Atleta{
+    private string $nome;
+    private int $idade;
+    private string $esporte;
+    private Pais $pais;
+    
+}
+?>
---
+++ POO/2_Bimestre/aula_11/PaisAtletas/modelos/Pais.php
@@ -0,0 +1,45 @@
+<?php 
+
+class Pais {
+    private string $nome;
+    private string $continente;
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of continente
+     */
+    public function getContinente(): string
+    {
+        return $this->continente;
+    }
+
+    /**
+     * Set the value of continente
+     */
+    public function setContinente(string $continente): self
+    {
+        $this->continente = $continente;
+
+        return $this;
+    }
+}
+
+
+?>
