ae2547ca3471a6270bf938b1f70d9f295c8a66e2
Kassime <134729407+GiKassime@users.noreply.github.com>
2024-12-16 14:24:04 -0300

    Update InnerJoin.sql

--- Banco de Dados/Exercicios-Trabalhos/InnerJoin.sql
+++ Banco de Dados/Exercicios-Trabalhos/InnerJoin.sql
@@ -10,9 +10,14 @@ cargaHoraria int not null
 create table Aluno (
 id_aluno int primary key,
 nome varchar(100) not null,
-email varchar(100),
+email varchar(100)
+);
+
+create table Matricula (
+id_aluno int ,
 id_curso int,
-foreign key (id_curso) references Curso(id_curso)
+foreign key (id_curso) references Curso(id_curso),
+foreign key (id_aluno) references Aluno(id_aluno)
 );
 
 insert into Curso 
@@ -22,14 +27,34 @@ values
 (3,'Desenvolvimento Web',45);
 
 insert into Aluno values 
-(1,'Guilherme','guilherme@gmail.com',1),
-(2,'Bianca','bianca@gmail.com',2),
-(3,'Livia','livia@gmail.com',3);
+(1,'Guilherme','guilherme@gmail.com'),
+(2,'Bianca','bianca@gmail.com'),
+(3,'Livia','livia@gmail.com');
+
+insert into Matricula values 
+(1,2),
+(2,3),
+(3,1);
 
 SELECT
 Aluno.nome as Nome, 
 Curso.nome as Nome_Curso
-FROM Aluno
-INNER JOIN Curso 
-ON Aluno.id_curso = Curso.id_curso;
+FROM Matricula
+INNER JOIN Aluno
+ON Matricula.id_aluno = Aluno.id_aluno
+INNER JOIN Curso
+ON Matricula.id_curso = Curso.id_curso;
+
+
+
+
+
+
+
+    
+
+
+
+
+
 
